591388a1f5dd302b98b397a0d70c8a27aad534eb
Macbook <macbook@macbooks-mbp.dsldevice.lan>
2018-08-21 08:52:07 +0700

    finnal

--- js/script.js
+++ js/script.js
@@ -1,87 +1,127 @@
-var cards = [ 'c10', 'c11', 'c12', 'c13',
-			 'd10', 'd11', 'd12', 'd13',
-			 'h10', 'h11', 'h12', 'h13',
-			 's10', 's11', 's12', 's13',
-			];
-var current = null;
+'use strict';
 
-function shuffle(array){
-	var currentIndex = array.length;
-	var randomIndex;
-	var temporaryvalue;
+var cardsArray = [{
+  'name': 'h01',
+  'img': 'img/trump/png/h01.png'
+}, {
+  'name': 'h02',
+  'img': 'img/trump/png/h02.png'
+}, {
+  'name': 'h03',
+  'img': 'img/trump/png/h03.png'
+}, {
+  'name': 'h04',
+  'img': 'img/trump/png/h04.png'
+}, {
+  'name': 'h05',
+  'img': 'img/trump/png/h05.png'
+}, {
+  'name': 'h06',
+  'img': 'img/trump/png/h06.png'
+}, {
+  'name': 'h07',
+  'img': 'img/trump/png/h07.png'
+}, {
+  'name': 'h08',
+  'img': 'img/trump/png/h08.png'
+}, {
+  'name': 'h09',
+  'img': 'img/trump/png/h09.png'
+}, {
+  'name': 'h10',
+  'img': 'img/trump/png/h10.png'
+}, {
+  'name': 'h11',
+  'img': 'img/trump/png/h11.png'
+}, {
+  'name': 'h12',
+  'img': 'img/trump/png/h12.png'
+}, {
+  'name': 'h13',
+  'img': 'img/trump/png/h13.png'
+}];
 
-	//While there remain elements to shuffle
-	while(0 != currentIndex){
+var gameGrid = cardsArray.concat(cardsArray).sort(function () {
+  return 0.5 - Math.random();
+});
 
-		//Pick a remaining element
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
+var firstGuess = '';
+var secondGuess = '';
+var count = 0;
+var previousTarget = null;
+var delay = 1200;
 
-		//And swap it with the current element
-		temporaryvalue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryvalue;
-	}
+var game = document.getElementById('game');
+var grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
 
-	return array;
-}
+gameGrid.forEach(function (item) {
+  var name = item.name,
+      img = item.img;
 
-// var arr = shuffle(cards);
-// console.log(arr);
 
-function flig(card){
-	// $(card).flip({
- //  		trigger: 'click'
-	// 	});
-	$(card).flip();
+  var card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
 
-	if(current==null){
-		current = $(card);
-	}else{
-		if(current.attr('data-name') != $(card).attr('data-name')){
-			//Khac nhau
-			setTimeout(function(){
-				console.log('Khác nhau');
-				// current.toggleClass('flipped');
-				// $(card).toggleClass('flipped');
-				$(card).flip(false);
-				$(current).flip(false);
-				current = null;
-			}, 1000);
-		}else{
-			//Giong nhau
-			setTimeout(function(){
-				console.log('Giống nhau');
-				$(card).css('opacity', '0');
-				current.css('opacity', '0');
-				current = null;
-			},500);
-		}
-	}
-}
+  var front = document.createElement('div');
+  front.classList.add('front');
 
-$(function() {
-	//Nhân đôi mảng để tạo ra các cặp bài
-	cards = cards.concat(cards);
+  var back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = 'url(' + img + ')';
 
-	//Đảo vị trí các quân bài
-	cards = shuffle(cards);
-	console.log(cards);
-	//Chèn nội dung các quân bài vào trong element có class ="content"
-	var html = '';
-	for (var i=0; i<cards.length; i++){
-		html += 
-			'<div class="grid">' +
-				'<div class="card" data-name="'+cards[i]+'" onclick="flig(this)">' +
-					'<div class="front">'+
-						'<img src="trump/png/x01.png">'+
-					'</div>'+
-					'<div class="back">'+
-						'<img src="trump/png/'+cards[i]+'.png">'+
-					'</div>'+
-				'</div>'+
-			'</div>';
-	};
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
 
-	$('.content').html(html);
+var match = function match() {
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.add('match');
+  });
+};
+
+var resetGuesses = function resetGuesses() {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.remove('selected');
+  });
+};
+
+grid.addEventListener('click', function (event) {
+
+  var clicked = event.target;
+
+  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('selected') || clicked.parentNode.classList.contains('match')) {
+    return;
+  }
+
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
+      clicked.parentNode.classList.add('selected');
+    }
+
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
 });
